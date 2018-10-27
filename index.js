8bcf7e92a759a59b57acc17b64f56e4e269fa615
Alexa Harmon <alexa.angelina7@gmail.com>
2018-10-27 17:01:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-var uppercase, lowercase
+var uppercase, lowercase;
  
 function shout(string){
   uppercase = string.toUpperCase();
@@ -44,12 +11,12 @@ function whisper(string){
   return lowercase;
 }
 function logShout(string){
-  uppercase = string.toUpperCase()
+  uppercase = string.toUpperCase();
   string = uppercase;
   console.log(uppercase);
 }
 function logWhisper(string){
-  lowercase = string.toLowerCase()
+  lowercase = string.toLowerCase();
   string = uppercase;
   console.log(lowercase);
 }
